09d7e682a9824b8452fa3c8ed59317cc6d06111c
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-09-10 11:33:51 +0300

    Adding some boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="wrap">
+        <div className="card">
+          <div className="card-image">
+            <img src="./sea.jpg" alt="diving" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
