9f051e587992e8ea4e0f2f0108fb291690addc68
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-09-11 13:22:07 +0300

    Adding middleware

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import thunk from "redux-thunk";
 import { rootReducer } from "./redux/rootReducer";
 import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { spamFilter } from "./redux/middleware";
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, spamFilter),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
