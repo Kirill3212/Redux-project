9218a4554f444eb893e925d9b22816846b53c290
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-09-11 11:48:24 +0300

    Adding loader

---
+++ src/Spin.js
@@ -0,0 +1,22 @@
+import { ColorRing } from "react-loader-spinner";
+import { useSelector } from "react-redux";
+
+const Spin = () => {
+  const spinner = useSelector((state) => state.appReducer.loading);
+  console.log("spinner", spinner);
+  return (
+    <div className="loader-styles">
+      <ColorRing
+        visible={spinner}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
+    </div>
+  );
+};
+
+export default Spin;
